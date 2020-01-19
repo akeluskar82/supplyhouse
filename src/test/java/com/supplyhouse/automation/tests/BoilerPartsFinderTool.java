4b3cb240e3b197ff078f5c84affba40c1fe72a7c
Amit Keluskar <amitkeluskar@it1513522.lan>
2020-01-18 19:47:17 -0500

    Updated junit dependency to use junit5

--- src/test/java/com/supplyhouse/automation/tests/BoilerPartsFinderTool.java
+++ src/test/java/com/supplyhouse/automation/tests/BoilerPartsFinderTool.java
@@ -1,40 +1,43 @@
 package com.supplyhouse.automation.tests;
 
-import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
 import com.supplyhouse.automation.helperutils.WebDriverUtils;
 
 import pageobjects.BoilerPartsFinderToolPage;
 
-@RunWith(JUnitPlatform.class)
 public class BoilerPartsFinderTool {
+	private WebDriverUtils driverUtils;
 
 	@ParameterizedTest
 	@ValueSource(strings = { "AT082410C", "AT062310C", "K2WT-150" })
 	public void verifyBoilerFinderToolSearch(String modelNumber) {
 
-		WebDriverUtils driverUtils = new WebDriverUtils();
-		driverUtils.getDriverInstance();
-		driverUtils.loadUrl("https://www.supplyhouse.com/Boiler-Parts-Finder-Tool");
 		BoilerPartsFinderToolPage boilerPartsPage = new BoilerPartsFinderToolPage();
 		boilerPartsPage.modelNumberSearch().sendKeys(modelNumber);
 		boilerPartsPage.boilerPartsSearchButton().click();
+		assertTrue(boilerPartsPage.doesModelNumberAppearInLink(modelNumber),
+				"The search shows a result in which a link appears for the following model number:  " + modelNumber);
+		assertTrue(boilerPartsPage.doesModelNumberAppearInText(modelNumber),
+				"The search shows a result in which text appears for the following model number: " + modelNumber);
 
-		try {
-			Assert.assertTrue(
-					"The search shows a result in which a link appears for the following model number:  " + modelNumber,
-					boilerPartsPage.doesModelNumberAppearInLink(modelNumber));
-			Assert.assertTrue(
-					"The search shows a result in which text appears for the following model number: " + modelNumber,
-					boilerPartsPage.doesModelNumberAppearInText(modelNumber));
-		} finally {
-			driverUtils.closeDriver();
-		}
+	}
 
+	@BeforeEach
+	public void setupTest() {
+		driverUtils = new WebDriverUtils();
+		driverUtils.getDriverInstance();
+		driverUtils.loadUrl("https://www.supplyhouse.com/Boiler-Parts-Finder-Tool");
+	}
+
+	@AfterEach
+	public void closeDriver() {
+		driverUtils.closeDriver();
 	}
 
 }
